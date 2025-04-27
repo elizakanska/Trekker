764f59ef71e58aa46f7e1456e48c721e09640924
VinetaBitte <vineta.bitte@edu.rtu.lv>
2025-04-27 15:10:40 +0300

    Add database schema and data files

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,33 @@
+-- Table for users
+  CREATE TABLE users (
+    user_id INT NOT NULL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+-- Table for trails
+  CREATE TABLE trails (
+    trail_id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    location VARCHAR(150) NOT NULL,
+    type VARCHAR(30) NULL,
+    biome VARCHAR(30) NULL,
+    difficulty VARCHAR(20) NULL,
+    length DECIMAL(5,2) NULL
+);
+-- Table for multiple friends
+CREATE TABLE friends (
+    user1_id INT NOT NULL,
+    friend_id INT NOT NULL,
+    PRIMARY KEY (user1_id, friend_id),
+    FOREIGN KEY (user1_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (friend_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+-- Table for favorite trails
+CREATE TABLE favorites (
+    user_id INT NOT NULL,
+    trail_id INT NOT NULL,
+    PRIMARY KEY (user_id, trail_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (trail_id) REFERENCES trails(trail_id) ON DELETE CASCADE
+);
